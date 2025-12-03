d9c42fb98ae701ef62419a027d2f212331da8cd0
Forkentiney <jseverud@proton.me>
2025-12-03 10:49:42 -0600

    Footer for footnotes reformatted
    This seems to make sense to me, but time will tell. The mobile version
    does not work.

--- content/writing/footnotes.js
+++ content/writing/footnotes.js
@@ -1,10 +1,17 @@
 const markers = document.querySelectorAll(".marker");
 const footnotes = document.querySelectorAll(".footnote");
+const close = document.querySelector(".close");
+const notesFooter = document.querySelector(".footnotes");
 
 markers.forEach((marker) => {
   marker.addEventListener("click", () => {
     let i = marker.getAttribute("id");
     let footnote = document.getElementById(`foot${i}`);
     footnote.classList.toggle("hidden");
+    notesFooter.classList.remove("hidden");
   });
 });
+
+close.addEventListener("click", () => {
+  notesFooter.classList.add("hidden");
+})
