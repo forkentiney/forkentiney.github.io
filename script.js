0d4a0c73a9c96774c65ae36bf11ae96bc78c84cd
Forkentiney <jseverud@proton.me>
2026-05-04 13:46:26 -0500

    Added weather widget.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const header = document.querySelector("#header");
+const weather = document.querySelector("#weather")
+const condition = document.querySelector("#condition");
 
 // Add shadow on scroll down, remove it at top of page.
 window.addEventListener("scroll", () => {
@@ -8,3 +10,17 @@ window.addEventListener("scroll", () => {
     header.classList.add("shadow");
   }
 });
+
+(async () => {
+	const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/grand%20forks?key=LJVZ5T9ETAKSGFVR8K9ZHY69P');
+	response.json().then(function(response) {
+		console.log(response);
+		const temperature = Math.floor((response.currentConditions.temp - 32) / 1.8);
+		const conditions = response.currentConditions.conditions.toLowerCase();
+		weather.textContent = `it's ${temperature}°C`;
+		condition.textContent = `and ${conditions}`;
+	}).catch(function(error) {
+		weather.textContent = "it's probably cold";
+		condition.textContent = "...and windy."
+	});
+})();
