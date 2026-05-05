e71ae85c47d227f1194cf7385e0e47d8ef2ccce4
Forkentiney <jseverud@proton.me>
2026-05-05 15:12:37 -0500

    Reworked template.html and styled it
    This gets things ready to start making body and other pages.

---
+++ src/home.js
@@ -0,0 +1,13 @@
+(async () => {
+	const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/grand%20forks?key=LJVZ5T9ETAKSGFVR8K9ZHY69P');
+	response.json().then(function(response) {
+		console.log(response);
+		const temperature = Math.floor((response.currentConditions.temp - 32) / 1.8);
+		const conditions = response.currentConditions.conditions.toLowerCase();
+		weather.textContent = `it's ${temperature}°C`;
+		condition.textContent = `and ${conditions}`;
+	}).catch(function(error) {
+		weather.textContent = "it's probably cold";
+		condition.textContent = "...and windy."
+	});
+})();
--- src/main.js
+++ src/main.js
@@ -1,26 +1,5 @@
+import "./template.css";
+
 const header = document.querySelector("#header");
 const weather = document.querySelector("#weather")
 const condition = document.querySelector("#condition");
-
-// Add shadow on scroll down, remove it at top of page.
-window.addEventListener("scroll", () => {
-  if (window.scrollY < 5) {
-    header.classList.remove("shadow");
-  } else {
-    header.classList.add("shadow");
-  }
-});
-
-(async () => {
-	const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/grand%20forks?key=LJVZ5T9ETAKSGFVR8K9ZHY69P');
-	response.json().then(function(response) {
-		console.log(response);
-		const temperature = Math.floor((response.currentConditions.temp - 32) / 1.8);
-		const conditions = response.currentConditions.conditions.toLowerCase();
-		weather.textContent = `it's ${temperature}°C`;
-		condition.textContent = `and ${conditions}`;
-	}).catch(function(error) {
-		weather.textContent = "it's probably cold";
-		condition.textContent = "...and windy."
-	});
-})();
